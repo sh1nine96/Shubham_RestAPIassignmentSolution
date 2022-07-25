5980454fdf4fc40172f09787cf24300f55bbad42
Shubham Sharma <sh1nine96@gmail.com>
2022-07-25 05:46:40 +0530

    Add files via upload

---
+++ Graded4-ACPFSD-SpringBoot-RestAPI-EmployeeManagement/src/main/java/com/gl/javafsd/employeemanagement/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.gl.javafsd.employeemanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gl.javafsd.employeemanagement.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+	
+
+}
