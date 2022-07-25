0dd3765df2ec612ab762f9b0e2462b186dfbace1
Shubham Sharma <sh1nine96@gmail.com>
2022-07-25 05:45:13 +0530

    Add files via upload

---
+++ Graded4-ACPFSD-SpringBoot-RestAPI-EmployeeManagement/src/main/java/com/gl/javafsd/employeemanagement/service/RoleService.java
@@ -0,0 +1,9 @@
+package com.gl.javafsd.employeemanagement.service;
+
+import com.gl.javafsd.employeemanagement.model.Role;
+
+public interface RoleService {
+
+	String addNewRole(Role newRole);
+
+}
